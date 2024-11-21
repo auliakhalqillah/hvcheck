d1859c33def285d73de50e3e49d8e34950787f06
Aulia Khalqillah <52442929+auliakhalqillah@users.noreply.github.com>
2024-11-21 11:58:02 +0700

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as fh:
 
 setuptools.setup(
     name="hvcheck",
-    version="0.0.1",
+    version="0.2",
     author="Aulia Khalqillah",
     author_email="auliakhalqillah.mail@gmail.com",
     description="Program to check reliable and clear peak of H/V curve",
